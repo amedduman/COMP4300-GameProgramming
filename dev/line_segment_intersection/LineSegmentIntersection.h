d3fe2a5047e83bdfbfc00116c603782ef7135745
amedduman <amedduman@protonmail.com>
2023-12-25 02:16:58 +0300

    working on intersect point

--- dev/line_segment_intersection/LineSegmentIntersection.h
+++ dev/line_segment_intersection/LineSegmentIntersection.h
@@ -16,30 +16,68 @@ public:
     }
     void Run()
     {
+        auto circle = sf::CircleShape(4);
+        circle.setOrigin(4,4);
+        circle.setFillColor(sf::Color::Transparent);
+        circle.setOutlineThickness(1);
+
+        auto a1 = sf::Vector2f(400, 500);
+        auto a2 = sf::Vector2f(550, 150);
         sf::Vertex lineA[] =
         {
-            sf::Vertex(sf::Vector2f(100, 100)),
-            sf::Vertex(sf::Vector2f(150, 150))
+            sf::Vertex(a1),
+            sf::Vertex(a2)
         };
+        lineA[0].color = sf::Color::Red;
+        lineA[1].color = sf::Color::Red;
 
+        auto b1 = sf::Vector2f(100, 200);
+        auto b2 = sf::Vector2f(250, 250);
         sf::Vertex lineB[] =
         {
-            sf::Vertex(sf::Vector2f(100, 200)),
-            sf::Vertex(sf::Vector2f(250, 250))
+            sf::Vertex(b1),
+            sf::Vertex(b2)
         };
 
         while (m_window.isOpen())
         {
             SInput();
+
+            b2.x = sf::Mouse::getPosition(m_window).x;
+            b2.y = sf::Mouse::getPosition(m_window).y;
+            lineB[1] = sf::Vertex(sf::Vector2f(b2));
+
+            auto intersectionPoint = LineIntersection(a1, a2, b1, b2);
+            circle.setPosition(intersectionPoint);
+
             m_window.clear(sf::Color(25, 35,25));
 
             m_window.draw(lineA, 2, sf::Lines);
             m_window.draw(lineB, 2, sf::Lines);
+            m_window.draw(circle);
 
             m_window.display();
         }
     }
 private:
+    sf::Vector2f LineIntersection(sf::Vector2f p0, sf::Vector2f p1, sf::Vector2f p2, sf::Vector2f p3)
+    {
+        float a1 = p1.y - p0.y;
+        float b1 = p0.x - p1.x;
+        float c1 = a1 * p0.x + b1 * p0.y;
+
+        float a2 = p3.y - p2.y;
+        float b2 = p2.x - p3.x;
+        float c2 = a2 * p2.x + b2 * p2.y;
+
+        float denominator = a1 * b2 - a2 * b1;
+
+        auto intersect = sf::Vector2f(0,0);
+        intersect.x = (b2 * c1 - b1 * c2) / denominator;
+        intersect.y = (a1 * c2 - a2 * c1) / denominator;
+
+        return intersect;
+    }
     void SRender()
     {
         m_window.clear(sf::Color(25, 35,25));
