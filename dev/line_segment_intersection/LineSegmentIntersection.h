37260bdbd46baab05546bb88add4d3c3833912b5
amedduman <amedduman@protonmail.com>
2023-12-26 01:12:13 +0300

    add multiple shapes to ray cast

--- dev/line_segment_intersection/LineSegmentIntersection.h
+++ dev/line_segment_intersection/LineSegmentIntersection.h
@@ -3,8 +3,13 @@
 #include <SFML/Graphics/RenderWindow.hpp>
 #include "../../engine/ecs/EntityManager.h"
 
-struct LineIntersectionResult
+struct Intersect
 {
+    Intersect()
+    {
+        isIntersected = false;
+        intersectionPoint = sf::Vector2f(0,0);
+    }
     bool isIntersected;
     sf::Vector2f intersectionPoint;
 };
@@ -21,28 +26,25 @@ public:
     }
     void Run()
     {
+        //circle
         auto circle = sf::CircleShape(4);
         circle.setOrigin(4,4);
         circle.setFillColor(sf::Color::Transparent);
         circle.setOutlineThickness(1);
 
-        auto a1 = sf::Vector2f(400, 500);
+        // line
+        auto a1 = sf::Vector2f(400, 300);
         auto a2 = sf::Vector2f(550, 150);
-        sf::Vertex lineA[] =
+        sf::Vertex line[] =
         {
             sf::Vertex(a1),
             sf::Vertex(a2)
         };
-        lineA[0].color = sf::Color::Red;
-        lineA[1].color = sf::Color::Red;
+        line[0].color = sf::Color::Red;
+        line[1].color = sf::Color::Red;
 
-        auto b1 = sf::Vector2f(400, 600);
-        auto b2 = sf::Vector2f(550, 250);
-        sf::Vertex lineB[] =
-        {
-            sf::Vertex(b1),
-            sf::Vertex(b2)
-        };
+        // rects
+        std::vector<sf::RectangleShape> rects;
 
         sf::RectangleShape rect1;
         rect1.setSize(sf::Vector2f(100,100));
@@ -51,52 +53,35 @@ public:
         rect1.setFillColor(sf::Color::Transparent);
 
         sf::RectangleShape rect2;
-        rect2.setSize(sf::Vector2f(300,100));
-        rect2.setPosition(300,300);
+        rect2.setSize(sf::Vector2f(100,100));
+        rect2.setPosition(100,300);
         rect2.setOutlineThickness(1);
         rect2.setFillColor(sf::Color::Transparent);
 
-        /*
-        sf::Vector2f a = sf::Vector2f(rect.getPosition().x + rect.getPoint(0).x, rect.getPosition().y + rect.getPoint(0).y);
-        sf::Vector2f b= sf::Vector2f(rect.getPosition().x + rect.getPoint(1).x, rect.getPosition().y + rect.getPoint(1).y);
-        sf::Vector2f c= sf::Vector2f(rect.getPosition().x + rect.getPoint(2).x, rect.getPosition().y + rect.getPoint(2).y);
-        sf::Vector2f d= sf::Vector2f(rect.getPosition().x + rect.getPoint(3).x, rect.getPosition().y + rect.getPoint(3).y);
-        // std::cout << a.x << ", " << a.y << std::endl;
-        // std::cout << b.x << ", " << b.y << std::endl;
-        // std::cout << c.x << ", " << c.y << std::endl;
-        // std::cout << d.x << ", " << d.y << std::endl;
-        sf::Vertex ab[] =
-        {
-            sf::Vertex(a),
-            sf::Vertex(b),
-        };
-        sf::Vertex bc[] =
-        {
-            sf::Vertex(b),
-            sf::Vertex(c),
-        };
-        sf::Vertex cd[] =
-        {
-            sf::Vertex(c),
-            sf::Vertex(d),
-        };
-        sf::Vertex da[] =
-        {
-            sf::Vertex(d),
-            sf::Vertex(a),
-        };
-        */
+        sf::RectangleShape screenBounds;
+        screenBounds.setSize(sf::Vector2f(m_window.getSize().x,m_window.getSize().y));
+        screenBounds.setPosition(0,0);
+        screenBounds.setOutlineThickness(1);
+        screenBounds.setFillColor(sf::Color::Transparent);
+
+        rects.push_back(rect1);
+        rects.push_back(rect2);
+        rects.push_back(screenBounds);
 
         while (m_window.isOpen())
         {
             SInput();
 
-            b2.x = sf::Mouse::getPosition(m_window).x;
-            b2.y = sf::Mouse::getPosition(m_window).y;
-            lineB[1] = sf::Vertex(sf::Vector2f(b2));
+            a2.x = sf::Mouse::getPosition(m_window).x;
+            a2.y = sf::Mouse::getPosition(m_window).y;
+            line[1] = sf::Vertex(sf::Vector2f(a2));
 
-            // auto intersectionResult = LineSegIntersect(a1, a2, b1, b2);
-            auto intersectionResult = LineVsRect(b1,b2, rect1);
+            Intersect intersectionResult;
+            for(auto& e : rects)
+            {
+                intersectionResult = LineVsRect(a1,a2, e);
+                if (intersectionResult.isIntersected) break;
+            }
             if (intersectionResult.isIntersected)
             {
                 circle.setPosition(intersectionResult.intersectionPoint);
@@ -104,86 +89,22 @@ public:
             else
                 circle.setPosition(sf::Vector2f(0,0));
 
-            // LineVsRect(b1,b2, rect);
-
             m_window.clear(sf::Color(25, 35,25));
 
-            m_window.draw(lineA, 2, sf::Lines);
-            m_window.draw(lineB, 2, sf::Lines);
+            m_window.draw(line, 2, sf::Lines);
             m_window.draw(circle);
-            m_window.draw(rect1);
-            // m_window.draw(ab, 4, sf::Lines);
-            // m_window.draw(bc, 4, sf::Lines);
-            // m_window.draw(cd, 4, sf::Lines);
-            // m_window.draw(da, 4, sf::Lines);
+            for(auto& e : rects)
+            {
+                m_window.draw(e);
+            }
 
             m_window.display();
         }
     }
 private:
-    LineIntersectionResult LineSegIntersect(sf::Vector2f a, sf::Vector2f b, sf::Vector2f c, sf::Vector2f d)
+    Intersect LineVsRect(sf::Vector2f p0, sf::Vector2f p1, sf::RectangleShape rect)
     {
-        LineIntersectionResult result;
-
-        Vec2 r = Vec2(b.x - a.x, b.y - a.y);
-        Vec2 s = Vec2(d.x - c.x, d.y - c.y);
-        float rxs = Cross(r, s);
-        Vec2 cma = Vec2(c.x - a.x, c.y - a.y);
-
-        float t = Cross(cma, s) / rxs;
-        float u = Cross(cma, r) / rxs;
-
-        if(t >= 0 && t <= 1 && u >= 0 && u <= 1)
-        {
-            result.isIntersected = true;
-            result.intersectionPoint = sf::Vector2f(a.x + t*r.x, a.y + t*r.y);
-        }
-        else
-        {
-            result.isIntersected = false;
-            result.intersectionPoint = sf::Vector2f(0,0);
-        }
-
-        return result;
-    }
-    float Cross(Vec2 a, Vec2 b)
-    {
-        return a.x * b.y - a.y * b.x;
-    }
-    LineIntersectionResult LineIntersection(sf::Vector2f p0, sf::Vector2f p1, sf::Vector2f p2, sf::Vector2f p3)
-    {
-        LineIntersectionResult result;
-
-        float a1 = p1.y - p0.y;
-        float b1 = p0.x - p1.x;
-        float c1 = a1 * p0.x + b1 * p0.y;
-
-        float a2 = p3.y - p2.y;
-        float b2 = p2.x - p3.x;
-        float c2 = a2 * p2.x + b2 * p2.y;
-
-        float denominator = a1 * b2 - a2 * b1;
-        if(denominator == 0)
-        {
-            result.isIntersected = false;
-
-            result.intersectionPoint = sf::Vector2f(0,0);
-        }
-        else
-        {
-            result.isIntersected = true;
-            auto intersect = sf::Vector2f(0,0);
-            intersect.x = (b2 * c1 - b1 * c2) / denominator;
-            intersect.y = (a1 * c2 - a2 * c1) / denominator;
-
-            result.intersectionPoint = intersect;
-        }
-
-        return result;
-    }
-    LineIntersectionResult LineVsRect(sf::Vector2f p0, sf::Vector2f p1, sf::RectangleShape rect)
-    {
-        LineIntersectionResult result;
+        Intersect result;
         result.isIntersected = false;
         result.intersectionPoint = sf::Vector2f(0,0);
         sf::Vector2f a = sf::Vector2f(rect.getPosition().x + rect.getPoint(0).x, rect.getPosition().y + rect.getPoint(0).y);
@@ -216,19 +137,38 @@ private:
         }
         return result;
     }
-    void SRender()
+
+    Intersect LineSegIntersect(sf::Vector2f a, sf::Vector2f b, sf::Vector2f c, sf::Vector2f d)
     {
-        m_window.clear(sf::Color(25, 35,25));
-        for (auto& e : m_entityManager.GetComponents<CShape>())
+        Intersect result;
+
+        Vec2 r = Vec2(b.x - a.x, b.y - a.y);
+        Vec2 s = Vec2(d.x - c.x, d.y - c.y);
+        float rxs = Cross(r, s);
+        Vec2 cma = Vec2(c.x - a.x, c.y - a.y);
+
+        float t = Cross(cma, s) / rxs;
+        float u = Cross(cma, r) / rxs;
+
+        if(t >= 0 && t <= 1 && u >= 0 && u <= 1)
         {
-            m_window.draw(e->rect);
+            result.isIntersected = true;
+            result.intersectionPoint = sf::Vector2f(a.x + t*r.x, a.y + t*r.y);
         }
-        for (auto& e : m_entityManager.GetComponents<CSprite>())
+        else
         {
-            m_window.draw(e->GetSprite());
+            result.isIntersected = false;
+            result.intersectionPoint = sf::Vector2f(0,0);
         }
-        m_window.display();
+
+        return result;
     }
+
+    float Cross(Vec2 a, Vec2 b)
+    {
+        return a.x * b.y - a.y * b.x;
+    }
+
     void SInput()
     {
         sf::Event event {};
